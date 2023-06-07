d84d7b2605a8b594803ee98b2ed02e0e266224db
daidai <dd891688848@163.com>
2023-06-07 19:43:48 +0800

    update

--- public/_nuxt/decimal.23b4f3b3.js
+++
@@ -1,1 +0,0 @@
-import{D as o}from"./pool.9fdab3ba.js";o.config({precision:64,rounding:o.ROUND_DOWN,toExpNeg:-64,toExpPos:64});
--- public/_nuxt/icon-Rewards.e802d0fb.js
+++
@@ -1,1 +0,0 @@
-import"./entry.1a05a197.js";const e=""+new URL("icon-Rewards.add64e7c.png",import.meta.url).href;export{e as _};
