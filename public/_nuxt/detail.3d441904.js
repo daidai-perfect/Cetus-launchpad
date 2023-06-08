a344024d4b3e6eaa1fee1ac4417aac665675c500
daidai <dd891688848@163.com>
2023-06-08 10:34:50 +0800

    update

--- public/_nuxt/detail.3d441904.js
+++
@@ -1,1 +0,0 @@
-import{af as e}from"./entry.a66dad04.js";const l=e((t,d)=>{const o=document.querySelector(".body-container-bg");console.log(o,"body##"),o&&o.scrollTo({top:0})});export{l as default};
