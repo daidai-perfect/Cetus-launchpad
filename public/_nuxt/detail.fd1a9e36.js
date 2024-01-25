ac5c683c55ff85fdf365a28289c24e820772c8e5
daidai <dd891688848@163.com>
2024-01-25 14:54:05 +0800

    更新

---
+++ public/_nuxt/detail.fd1a9e36.js
@@ -0,0 +1,1 @@
+import{fb as e}from"./entry.7346a543.js";const c=e((t,r)=>{const o=document.querySelector(".body-container-bg");console.log(o==null?void 0:o.scrollHeight,"scrollHeight#"),o&&o.scrollTo({top:0})});export{c as default};
---
+++ public/_nuxt/icon_rewards_2x.d35fa2d2.js
@@ -0,0 +1,1 @@
+import"./entry.7346a543.js";const i=""+globalThis.__publicAssetsURL("sui-image/icon_rewards@2x.png");export{i as _};
