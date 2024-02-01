c6e68db136bdb094e225786e83a71b512d694176
daidai <dd891688848@163.com>
2024-02-01 18:30:57 +0800

    更新

---
+++ public/_nuxt/detail.c26637b8.js
@@ -0,0 +1,1 @@
+import{W as e}from"./entry.711abf65.js";const c=e((t,r)=>{const o=document.querySelector(".body-container-bg");console.log(o==null?void 0:o.scrollHeight,"scrollHeight#"),o&&o.scrollTo({top:0})});export{c as default};
--- public/_nuxt/index.dc4b6931.js
+++
@@ -1,1 +0,0 @@
-import o from"./launchpad.f6e31514.js";import"./index.8449c50f.js";import"./entry.3eb1e501.js";import"./index.02b28798.js";import"./index.1e3944db.js";import"./useTheme.016cd3b3.js";const c=o;export{c as default};
