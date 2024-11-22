4aaeca7d4bc6afb9941cac28349d5b908428dade
daidai <dd891688848@163.com>
2024-11-22 19:55:07 +0800

    更新

--- public/assets/useJump2Dex-CIZnbRNC.js
+++
@@ -1,2 +0,0 @@
-import{p}from"./App-wxcbb-1s.js";function m(){const{cetus_clmm_site:o}=p;return{back2Dex:()=>{window.open(`${o}`,"_self")},jump2Swap:(e,s)=>{window.open(`${o}/swap?from=${e}&to=${s}`,"_self")},jump2Pool:e=>{window.open(`${o}/liquidity/deposit?poolAddress=${e}`,"_self")}}}export{m as u};
-//# sourceMappingURL=useJump2Dex-CIZnbRNC.js.map
---
+++ public/assets/useJump2Dex-KdS8Oaj_.js
@@ -0,0 +1,2 @@
+import{p}from"./App-BA1bk3ZQ.js";function m(){const{cetus_clmm_site:o}=p;return{back2Dex:()=>{window.open(`${o}`,"_self")},jump2Swap:(e,s)=>{window.open(`${o}/swap?from=${e}&to=${s}`,"_self")},jump2Pool:e=>{window.open(`${o}/liquidity/deposit?poolAddress=${e}`,"_self")}}}export{m as u};
+//# sourceMappingURL=useJump2Dex-KdS8Oaj_.js.map
